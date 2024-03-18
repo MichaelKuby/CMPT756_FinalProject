ad234fb9e1d03e763b0b064230b867111b3e231d
MichaelKuby <105996614+MichaelKuby@users.noreply.github.com>
2024-03-18 14:52:48 -0700

    Update matrixMult.py

--- matrixMult.py
+++ matrixMult.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 # To run from the command line:
 # time python matrixMult.py s or m or l
@@ -32,26 +33,31 @@ class MatrixMult():
                 for k in range(cols_A):
                     result[i][j] += A[i][k] * B[k][j]
                     
-        return print('Executed successfully')
+        return
 
 if __name__ == "__main__":
+    start_time = time.perf_counter()
     # get input arg from command line
     mode = sys.argv[1]
     
     if mode == 's':
         # compute a 3 * 3 matrix multiplication
-        A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-        B = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
+        A = [[i for i in range(5)] for _ in range(5)]
+        B = [[i for i in range(5)] for _ in range(5)]
     elif mode == 'm':
         # compute a 500 * 500 matrix multiplication
         A = [[i for i in range(500)] for _ in range(500)]
         B = [[i for i in range(500)] for _ in range(500)]
     elif mode == 'l':
         # compute a 10000 * 10000 matrix multiplication
-        A = [[i for i in range(10000)] for _ in range(10000)]
-        B = [[i for i in range(10000)] for _ in range(10000)]
+        A = [[i for i in range(1000)] for _ in range(1000)]
+        B = [[i for i in range(1000)] for _ in range(1000)]
     else:
         print("Invalid mode. Please use 's' for small, 'm' for medium, or 'l' for large.")
         sys.exit()
 
-    print(MatrixMult.matrix_multiplication(A, B))
+    MatrixMult.matrix_multiplication(A, B)
+    
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+    print(f"The code executed in {elapsed_time} seconds.")
